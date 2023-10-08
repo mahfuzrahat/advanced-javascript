ef186f945b367920059793f6ec9f57598c25c4df
mahfuzrahat <mahfuzrahat@gmail.com>
2023-10-08 13:03:25 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// ........ those are Falsy.......
+// false
+//0
+// ""
+// undefined
+// Null
+// NaN
+// ...... those are truthy......
+// "0", " ", [], {}
+
+
+let name = "false";
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
