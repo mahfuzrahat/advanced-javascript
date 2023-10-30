f011a4be4e87273cfa444789ebefae67dd2cb4da
mahfuzrahat <mahfuzrahat@gmail.com>
2023-10-30 14:17:44 +0600

    rest of the code added

---
+++ scope.js
@@ -0,0 +1,18 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if( result > 9){
+        var mood = "Happy";
+        mood = "frunky";
+        mood = "super happy";
+        // console.log(mood);
+    }
+    console.log(day);
+    var day = "friday";
+    return result;
+}
+const output = sum(3, 7);
+
+// console.log(output);
